c07a17e0181c1f8bfda7a497ebf9a317db0b3e43
littlelittle_in <littlelittle_in@littlelittle-inui-MacBookPro.local>
2021-02-10 18:04:30 +0900

    fix

--- server.js
+++ server.js
@@ -2,12 +2,14 @@ var express = require('express');
 const path = require('path');
 var app = express();
 var http = require("http").createServer(app);
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
 
 var cors = require("cors");
 
 app.use(cors());
 
-app.use(express.static("./build"))
+app.use(express.static("./build"),createProxyMiddleware({ target: 'http://172.16.11.7:8080', changeOrigin: true }))
 
 // 서버리슨
 http.listen(3000, () => {
