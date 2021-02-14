3999b144702d469223fc948cde34f645e0d9dd7c
artist-developer <artist.develops@gmail.com>
2021-02-14 19:13:02 +0900

    insert loadbalancer.

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var cors = require("cors");
 
 app.use(cors());
 
-app.use(express.static("./build"),createProxyMiddleware({ target: 'http://172.16.11.7:8080', changeOrigin: true }))
+app.use(express.static("./build"),createProxyMiddleware({ target: 'http://172.16.120.6:8080', changeOrigin: true }))
 
 // 서버리슨
 http.listen(3000, () => {
