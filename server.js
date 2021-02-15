1806b49a7c784345048e6cdab2a7c59b17382261
artist-developer <artist.develops@gmail.com>
2021-02-15 13:05:05 +0900

    update proxy.

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ var cors = require("cors");
 
 app.use(cors());
 
-app.use(express.static("./build"),createProxyMiddleware({ target: 'http://172.16.120.6:8080', changeOrigin: true }))
-
+app.use(express.static("./build"),createProxyMiddleware({ target: 'http://172.16.120.6:8080', changeOrigin: true }));
+//app.use(express.static("./build"));
 // 서버리슨
 http.listen(3000, () => {
   console.log("Server listening on port 3000");
--- src/App.js
+++ src/App.js
@@ -39,7 +39,9 @@ export default class App extends Component {
     this.getWeather();
   }
   getWeather = async () => {
-    const { data, error } = await axios.get("/get-weather");
+   console.log("send to server");	  
+   //const { data, error } = await axios.get("http://172.16.120.6:8080/get-weather");
+   const { data, error } = await axios.get("/get-weather");
 
     if (error) {
       console.log(error);
